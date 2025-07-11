4e0fb8c692cfce274da5f2d0dcdcbf5147df1b76
Anupama MN <motionsensor2025@gmail.com>
2025-07-12 00:03:17 +0530

    motin sensor

--- Motion_Sensor_2025.X/Motion_detection.X/build/default/production/service/sensor_service.p1.d
+++
@@ -1,3 +0,0 @@
-build/default/production/service/sensor_service.p1:  \
-service/sensor_service.c  \
-service/../hal/hal_sensor.h 
--- Motion_Sensor_2025.X/build/default/production/hal/hal_sensor.p1.d
+++
@@ -1,4 +0,0 @@
-build/default/production/hal/hal_sensor.p1:  \
-hal/hal_sensor.c  \
-hal/../mcc_generated_files/adc1.h  \
-hal/../mcc_generated_files/pin_manager.h 
